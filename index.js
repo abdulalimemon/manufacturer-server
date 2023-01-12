407c4c09e74d019c3224a48dd05a9852d004aad3
Emon02 <abdulalimemon11@gmail.com>
2023-01-12 22:34:43 +0600

    tools route added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -13,14 +14,30 @@ app.use(express.json());
 
 // MongoDB
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uzcobln.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 // APIs
-app.get('/', (req, res) => {
-    res.send('Running Server')
-});
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("manufacturer_user")
+        const toolscollection = database.collection("tools");
 
+        // Inventory API
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolscollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        });
 
-
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 // Port Listening
